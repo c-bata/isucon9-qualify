7e5ba5812325484261aaa9ceef89c518827701f1
c-bata <contact@c-bata.link>
2019-09-08 12:47:16 +0900

    Fix analysis script

--- analysis-from-local.sh
+++ analysis-from-local.sh
@@ -6,6 +6,6 @@ IPADDR=${IPADDR:-47.91.19.40}
 
 ssh -i $PEM ${USER}@${IPADDR} <<EOF
 sudo less /var/log/nginx/access.log | kataribe -f ~/isucari/kataribe.toml
-sudo /usr/bin/pt-query-digest /var/log/mariadb/mariadb-slow.log
+sudo /usr/bin/pt-query-digest /var/log/mysql/mysql-slow.log
 EOF
 

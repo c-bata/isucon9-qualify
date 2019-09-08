f131d516c8d462c3656dc3c91045c9860a98e483
c-bata <contact@c-bata.link>
2019-09-08 12:33:56 +0900

    Add analysis from local

---
+++ analysis-from-local.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+USER=${ISUCON_USER:-isucon}
+PEM=${PEM:-~/.ssh/isucon9.pem}
+IPADDR=${IPADDR:-47.91.19.40}
+
+ssh -i $PEM ${USER}@${IPADDR} <<EOF
+sudo less /var/log/nginx/access.log | kataribe -f ~/isucari/kataribe.toml
+sudo /usr/bin/pt-query-digest /var/log/mariadb/mariadb-slow.log
+EOF
+

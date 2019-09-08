dd1be3cc944adad04aaf67897ee6d9ce880a100a
gotyoooo <lss.ken8927@gmail.com>
2019-09-08 15:09:43 +0900

    fix sql

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -351,8 +351,8 @@ func getNewItems(w http.ResponseWriter, r *http.Request) {
 	if itemID > 0 && createdAt > 0 {
 		// paging
 		err := dbx.Select(&itemIDs,
-			"SELECT id FROM `items` WHERE `status` IN (?,?) AND (`created_at` < ?  OR (`created_at` <= ? AND `id` < ?)) ORDER BY `id` DESC LIMIT ?",
-			// "SELECT id FROM `items`, (SELECT item FROM `public_items` ORDER BY id DESC LIMIT ?) AS t WHERE t.item == items.id AND `status` IN (?,?) AND (`created_at` < ?  OR (`created_at` <= ? AND `id` < ?))",
+			// "SELECT id FROM `items` WHERE `status` IN (?,?) AND (`created_at` < ?  OR (`created_at` <= ? AND `id` < ?)) ORDER BY `id` DESC LIMIT ?",
+			"SELECT id FROM `items`, (SELECT item FROM `public_items` WHERE (`created_at` < ?  OR (`created_at` <= ? AND `id` < ?)) ORDER BY id DESC LIMIT ?) AS t WHERE t.item = items.id AND `status` IN (?,?)",
 			ItemStatusOnSale,
 			ItemStatusSoldOut,
 			time.Unix(createdAt, 0),
@@ -369,10 +369,10 @@ func getNewItems(w http.ResponseWriter, r *http.Request) {
 		// 1st page
 		err := dbx.Select(&itemIDs,
 			// "SELECT id FROM `items` WHERE `status` IN (?,?) ORDER BY `id` DESC LIMIT ?",
-			"SELECT id FROM `items`, (SELECT item FROM `public_items` ORDER BY id DESC LIMIT ?) AS t WHERE t.item == items.id AND `status` IN (?,?)",
+			"SELECT id FROM `items`, (SELECT item FROM `public_items` ORDER BY id DESC LIMIT ?) AS t WHERE t.item = items.id AND `status` IN (?,?)",
+			ItemsPerPage+1,
 			ItemStatusOnSale,
 			ItemStatusSoldOut,
-			ItemsPerPage+1,
 		)
 		if err != nil {
 			log.Print(err)
